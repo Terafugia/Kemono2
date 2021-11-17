6a8b3ed463727a460629a047323648debfa18223
Anonymous <>
2021-11-16 20:01:06 -0500

    fix(search): set random_page_cost on global queries

--- src/lib/posts.py
+++ src/lib/posts.py
@@ -62,7 +62,7 @@ def get_all_posts_for_query(q: str, offset: int, reload=False):
     results = redis.get(key)
     if results is None or reload:
         cursor = get_cursor()
-        query = "SET LOCAL enable_seqscan = off; SET LOCAL statement_timeout = 10000; "
+        query = "SET random_page_cost = 0.0001; SET LOCAL statement_timeout = 10000; "
         query += "SELECT * FROM posts WHERE title &@~ %s OR content &@~ %s ORDER BY added desc LIMIT 25 OFFSET %s"
         params = (q, offset)
 
