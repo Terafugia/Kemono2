7f1b522c210b799d4e17fbe49b3a41fa3d537572
Anonymous <>
2021-11-19 19:35:17 -0500

    feat(search): add titles to search

--- src/lib/posts.py
+++ src/lib/posts.py
@@ -31,8 +31,8 @@ def count_all_posts_for_query(q: str, reload=False):
     if count is None or reload:
         cursor = get_cursor()
         query = "SET random_page_cost = 0.0001; SET LOCAL statement_timeout = 10000; "
-        query += "SELECT COUNT(*) FROM posts WHERE content &@~ %s"
-        cursor.execute(query, (q,))
+        query += "SELECT COUNT(*) FROM ( SELECT * FROM posts WHERE content &@~ %s UNION SELECT * FROM posts WHERE title &@~ %s ) as UnionSearch;"
+        cursor.execute(query, (q, q))
         count = cursor.fetchone()
         redis.set(key, str(count['count']), ex=600)
         count = int(count['count'])
@@ -63,9 +63,8 @@ def get_all_posts_for_query(q: str, offset: int, reload=False):
     if results is None or reload:
         cursor = get_cursor()
         query = "SET random_page_cost = 0.0001; SET LOCAL statement_timeout = 10000; "
-        query += "SELECT * FROM posts WHERE content &@~ %s ORDER BY added desc LIMIT 25 OFFSET %s"
-        params = (q, offset)
-
+        query += "(SELECT * FROM posts WHERE content &@~ %s) UNION (SELECT * FROM posts WHERE title &@~ %s) ORDER BY added desc LIMIT 25 OFFSET %s;"
+        params = (q, q, offset)
         cursor.execute(query, params)
         results = cursor.fetchall()
         redis.set(key, serialize_dict_list(results), ex=600)
